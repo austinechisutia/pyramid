51d4753d4c898b1db5287c130d90637b3b2bbd9d
austinechisutia <austineakhonya624@gmail.com>
2025-01-26 16:44:16 +0300

    created the pyramid

--- script.js
+++ script.js
@@ -1,15 +1,20 @@
-const characters = '#';
+const character = "#";
 const count = 8;
 const rows = [];
 
-for (let i = 0; i < count; i++) {
-    rows.push(i)
-    
+
+for(let i = 0; i < count; i++) {
+    rows.push(padRow(i + 1, count));
+
 }
 
 let result = "";
-
-for(const row of rows){
-    result = result + row;
+for(const row of rows) {
+    result += row + "\n";
 }
+
 console.log(result);
+
+function padRow(rowNumber, rowCount){
+    return " ".repeat(rowCount - rowNumber) + character.repeat(2*rowNumber-1) + " ".repeat(rowCount - rowNumber);
+}
