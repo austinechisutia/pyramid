6f4e4e72390d35387cf5624d76e1c57bba37ba4f
austinechisutia <austineakhonya624@gmail.com>
2025-01-29 07:43:09 +0300

    for loop

--- script.js
+++ script.js
@@ -1,31 +1,17 @@
-const character = "#" // character to be printed
-const count = 8 // number of times to print the character
+const charater = "#";
+const count = 8;
 const rows = [];
 
 
-function createRow(num1, num2) {
-    return " ".repeat(num2 - num1) + character.repeat(2*num1-1);
-}
-
-// for (let i = 1; i < count; i++) {
-//     rows.push(createRow(i, count));
-// }
-
-
-while(rows.length<count) {
-    rows.push(createRow(rows.length + 1, count));
-    
-}
-for(let i = count; i> 0; i--){
-    rows.push(createRow(i, count));
+for(let i = 0; i<=count; i++){
+     rows.push(charater);
 }
 
 let result = "";
 
-for (const row of rows) {
+for(const row of rows){
     result += row + "\n";
-}
+   
 
-    
+}
 console.log(result);
-console.log(rows);
